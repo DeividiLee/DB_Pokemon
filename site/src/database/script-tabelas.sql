285a802db0e03d32c06eff187a9921bf895f3edd
DeividLee <davi.lee@sptech.school>
2023-10-21 23:49:40 -0300

    dashboard funcionando

--- site/src/database/script-tabelas.sql
+++ site/src/database/script-tabelas.sql
@@ -1,19 +1,11 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
 
-/*
-comandos para mysql - banco local - ambiente de desenvolvimento
-*/
-
-create database pokemon;
+create database if not exists pokemon;
 use pokemon;
-drop database pokemon;
 
 use pokemon;
 select * from todos_pokemon;
 alter table todos_pokemon add constraint primary key (idPokemon); -- precisa definir a pk na tabela todos_pokemon
-drop table todos_pokemon;
+
 
 CREATE TABLE usuario (
 	id INT PRIMARY KEY AUTO_INCREMENT,
@@ -21,8 +13,6 @@ CREATE TABLE usuario (
 	email VARCHAR(50),
 	senha VARCHAR(50)
 );
-
-select * from usuario;
 
 create table team_pokemon (
 idteam int auto_increment,
@@ -32,28 +22,16 @@ fkUsuario int,
 	  constraint FKU foreign key (fkUsuario) references usuario(id),
 primary key(idteam, fkPokemon, fkUsuario)
 );
-
+truncate table team_pokemon;
 select * from team_pokemon;
-drop table team_pokemon;
-drop table usuario;
+select * from usuario;
+
 
 insert into usuario values (
-null,'davi','davi.lee@sptech.school',1234567);
+null,'davi','davi.lee@sptech.school','12345678!');
+
+desc team_pokemon;
+
+SELECT * FROM team_pokemon join todos_pokemon on idPokemon = fkPokemon join usuario on id = fkusuario where id = 1;
 
 
-
-
-/*
-comandos para criar usuário em banco de dados azure, sqlserver,
-com permissão de insert + update + delete + select
-*/
-
-CREATE USER [usuarioParaAPIWebDataViz_datawriter_datareader]
-WITH PASSWORD = '#Gf_senhaParaAPIWebDataViz',
-DEFAULT_SCHEMA = dbo;
-
-EXEC sys.sp_addrolemember @rolename = N'db_datawriter',
-@membername = N'usuarioParaAPIWebDataViz_datawriter_datareader';
-
-EXEC sys.sp_addrolemember @rolename = N'db_datareader',
-@membername = N'usuarioParaAPIWebDataViz_datawriter_datareader';
--- site/src/models/pokemonModel.js
+++ site/src/models/pokemonModel.js
@@ -2,7 +2,8 @@ var database = require("../database/config")
 
 function listar() {
     var instrucao = `
-        SELECT * FROM team_pokemon join todos_pokemon on idPokemon = fkPokemon;
+        
+    SELECT * FROM team_pokemon join todos_pokemon on idPokemon = fkPokemon join usuario on id = fkusuario where id = 1;
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
@@ -15,8 +16,19 @@ function cadastrar(fkPokemon, idUsuario) {
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
 }
+function editar(novaDescricao, idAviso) {
+    console.log("ACESSEI O AVISO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function editar(): ", novaDescricao, idAviso);
+    var instrucao = `
+        UPDATE aviso SET descricao = '${novaDescricao}',  WHERE id = ${idAviso};
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
 
 module.exports = {
     cadastrar,
     listar
 };
+
+//// SELECT * FROM team_pokemon join todos_pokemon on idPokemon = fkPokemon;
+// SELECT * FROM team_pokemon join todos_pokemon on idPokemon = fkPokemon ORDER BY idteam DESC LIMIT 1 ;
